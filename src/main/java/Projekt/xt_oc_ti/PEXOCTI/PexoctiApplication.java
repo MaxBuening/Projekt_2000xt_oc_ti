903a2860135f00462018498f29fcf845004bf427
SynCr0Nic3 <buningmax@rocketmail.com>
2022-10-12 20:00:05 +0200

    v00001

---
+++ src/main/java/Projekt/xt_oc_ti/PEXOCTI/PexoctiApplication.java
@@ -0,0 +1,13 @@
+package Projekt.xt_oc_ti.PEXOCTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PexoctiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PexoctiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projekt/xt_oc_ti/PEXOCTI/PexoctiApplicationTests.java
@@ -0,0 +1,13 @@
+package Projekt.xt_oc_ti.PEXOCTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PexoctiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
