8bfa0db6ecb4733a53e19cfde07ec2818b503cda
KilianRuhfus <kilian.ruhfus@gmail.com>
2022-11-13 13:44:07 +0100

    Abgaenge fertig

--- src/main/java/Projekt/xt_oc_ti/PEXOCTI/AbgaengeRestController.java
+++ src/main/java/Projekt/xt_oc_ti/PEXOCTI/AbgaengeRestController.java
@@ -4,10 +4,11 @@ import Projekt.xt_oc_ti.PEXOCTI.api.Abgaenge;
 import Projekt.xt_oc_ti.PEXOCTI.api.AbgaengeManipulation;
 import Projekt.xt_oc_ti.PEXOCTI.service.AbgaengeService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 public class AbgaengeRestController {
@@ -18,10 +19,31 @@ public class AbgaengeRestController {
         this.abgaengeService = abgaengeService;
     }
 
-    @GetMapping(path = "/api/user/{id}/abgaenge")
-    public ResponseEntity<Abgaenge> createAbgang(@PathVariable Long id,
-                                                 @RequestBody AbgaengeManipulation request){
-        var Abgaenge = abgaengeService.create(request);
-        return null;
+    @PostMapping(path = "/api/user/{id}/abgaenge")
+    public ResponseEntity<Void> createAbgang(@PathVariable Long id, @RequestBody AbgaengeManipulation request) throws URISyntaxException {
+        var abgang = abgaengeService.create(request);
+        URI uri = new URI("/api/user/"+id.toString()+"/"+abgang.getId());
+        return ResponseEntity.created(uri).build();
     }
+
+    @GetMapping(path = "/api/user/abgaenge/{id}")
+    public ResponseEntity<Abgaenge> abgaengeById(@PathVariable Long id){
+        var abgang = abgaengeService.findById(id);
+        return abgang != null ? ResponseEntity.ok(abgang) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping(path = "api/user/abgaenge")
+    public ResponseEntity<List<Abgaenge>> abgaenge(){
+        return ResponseEntity.ok(abgaengeService.findAll());
+    }
+
+    // NOTE: deleteAbgang funktioniert nicht weil der Delete Pfad bei delete user irgendwie ähnlich genug ist,
+    //       dass der irgendwie auch ausgeführt wird. Führt zu einem Foreign Key constraint violation weil bie
+    //       Bankkonto der User ID irgendwie noch drin ist.
+
+    //@DeleteMapping(path = "/api/user/abgaenge/{id}")
+    //public ResponseEntity<Void> deleteAbgang(@PathVariable Long id){
+        //boolean success = abgaengeService.deleteById(id);
+        //return success ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    //}
 }
--- src/main/java/Projekt/xt_oc_ti/PEXOCTI/service/AbgaengeService.java
+++ src/main/java/Projekt/xt_oc_ti/PEXOCTI/service/AbgaengeService.java
@@ -7,27 +7,48 @@ import Projekt.xt_oc_ti.PEXOCTI.api.UserManipulationRequest;
 import Projekt.xt_oc_ti.PEXOCTI.persistence.AbgaengeEntity;
 import Projekt.xt_oc_ti.PEXOCTI.persistence.AbgaengeRepository;
 import Projekt.xt_oc_ti.PEXOCTI.persistence.UserEntity;
+import Projekt.xt_oc_ti.PEXOCTI.persistence.UserRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class AbgaengeService {
     private final AbgaengeRepository abgaengeRepository;
+    private final UserRepository userRepository;
 
-    public AbgaengeService(AbgaengeRepository abgaengeRepository){
+    public AbgaengeService(AbgaengeRepository abgaengeRepository, UserRepository userRepository){
         this.abgaengeRepository = abgaengeRepository;
+        this.userRepository = userRepository;
     }
+
     public List<Abgaenge> findAll(){
         List<AbgaengeEntity> abgaenge = abgaengeRepository.findAll();
         return abgaenge.stream().map(this::transformEntity)
                 .collect(Collectors.toList());
     }
 
+    public Abgaenge findById(Long id){
+        var abgaengeEntity = abgaengeRepository.findById(id);
+        return abgaengeEntity.map(this::transformEntity).orElse(null);
+    }
+
     public Abgaenge create(AbgaengeManipulation request){
-        //var AbgaengeEntity = new AbgaengeEntity(request.ge);
-        //userEntity = userRepository.save(userEntity);
-        //return transformEntity(userEntity);
-        return null;
+        var benutzer = userRepository.findById(request.getBenutzerID_Fk()).orElseThrow();
+        var abgangEntity = new AbgaengeEntity(benutzer, request.getAmount(), request.getBeschriftug(), request.getDatum());
+        abgangEntity = abgaengeRepository.save(abgangEntity);
+        return transformEntity(abgangEntity);
+    }
+
+    public boolean deleteById(Long id){
+        var abgaengeEntity = abgaengeRepository.findById(id);
+        if (userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
     private Abgaenge transformEntity(AbgaengeEntity abgaengeEntity){
