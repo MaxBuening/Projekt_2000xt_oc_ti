394a5490c9d7698d70008bccff00c7ccc5fab694
SynCr0Nic3 <buningmax@rocketmail.com>
2023-01-03 16:14:28 +0100

    zugang zur haupt daten tabelle gemacht

--- src/test/java/Projekt/xt_oc_ti/PEXOCTI/PexoctiApplicationTests.java
+++ src/test/java/Projekt/xt_oc_ti/PEXOCTI/PexoctiApplicationTests.java
@@ -1,16 +1,30 @@
 package Projekt.xt_oc_ti.PEXOCTI;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class PexoctiApplicationTests {
 
 	@Test
 	void contextLoads() {
-		String a = "hallo";
+		String hello = "hello";
+		assertEquals("hello",hello);
 
 
+	}
+
+	@Test
+	@DisplayName("Backend Testing Test 1")
+	void testTesting(){
+		String hello = "hello";
+		assertEquals("hello",hello);
 	}
 
 }
